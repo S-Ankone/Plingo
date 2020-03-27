44187a86b8b7302d4a297e8a9a6e997dc7af598f
Sjoerd Ankone <61697919+S-Ankone@users.noreply.github.com>
2020-03-27 14:13:24 +0100

    Stable Version 1.1
    This version is working, playable and presentable ! :)
    
    Not all ultimately desired features are present, but these would be mostly adding more time. 
    The goal of the assignment is reached, development is halted, though the game might be
    revisited in the future.

--- jscript/plingo_setup.js
+++ jscript/plingo_setup.js
@@ -1,25 +1,48 @@
 console.log("from the plingo_setup.js");
 
 // INIT gameCONTROLLER //
-let sendWord = document.getElementById("jsSendInput"); // get the send button
+let localisation = "Dutch";
+
+let input = document.getElementById("input");  // get the input text field/line
+let sendInput = document.getElementById("jsSendInput"); // get the send button
+
+let amountOfLetters = 6; // The amount of letters for this game session (**DEV: hard coded to 6 for Plingo)
+let startingLetter = "P"; // The starting letter for this game session (**DEV: hard coded to "p" for Plingo)
+const amountOfTurns = 4; //*** index starts at 0 ; The amount of turns people get (** DEV: hardcoded to be 5)
+let turn = 0; // the guessing turn, 1st try, 2nd try, etc.
+
+let score = 0; // keeping scores
+let points = 100; // points for a correct word
 
 let toGuess; // The game assigns the picked word to this variable
 let userGuess; // stores the word inputted by the user
-let turn = 0; // the guessing turn, 1st try, 2nd try, etc.
-const amountOfTurns = 5;  // The amount of turns people get (** DEV: hardcoded to be 5)
-let amountOfLetters = 6; // The amount of letters for this game session (**DEV: hard coded for Plingo)
-let startingLetter = "p"; // The starting letter for this game session (**DEV: hard coded for Plingo)
-let check; // a check variable Boolean for the do/while loop.
-let displayResult = " _ _ _ _ _ _ "; // a variable to display after evaluation of the words (**DEV: to be an Array of Letters)
+
 
 
 // INIT viewCONTROLLER //
-let input = document.getElementById("input");
-let eersteLetter = document.getElementsByClassName("eerste");
+let visualTimer = 865;
+let displayResult = " _ _ _ _ _ _ "; // a variable to display on console.log (**DEV: to be removed upon completion )
 
-let beginLetter = "P";  // (** DEV: beginLetter hardcoded voor Plingo als "P")
+buildBoard();
 
-for (i = 0; i < amountOfTurns; i++){
-	eersteLetter[i].textContent = beginLetter;
-	eersteLetter[i].classList.add("correct");
+function buildBoard(){
+
+	clearBoard();
+
+	let firstLetter = document.getElementsByClassName("first");
+	for (i = 0; i <= amountOfTurns; i++){
+		firstLetter[i].textContent = startingLetter;
+		firstLetter[i].classList.add("correct");
+	}
+}
+
+function clearBoard(){
+	let display = document.getElementsByClassName("letter");
+	for (i = 0; i < display.length; i++){
+		display[i].textContent = undefined;
+		display[i].classList.remove("correct");
+		display[i].classList.remove("almost");
+		display[i].classList.remove("wrong");
+		display[i].classList.remove("error");		
+	}
 }
--- jscript/plingo_viewController.js
+++ jscript/plingo_viewController.js
@@ -4,54 +4,54 @@ console.log("from viewController.js");
 // SEE SETUP FILE FOR VARIABLES INIT //
 // ********************************* //
 
-function setUserFeedback(toType){
-	let feedbackWindow = document.getElementById("user_feedback");
-	let showFeedback = document.getElementById("feedback");
-	switch(toType){
-		case "noLetters":
-			feedbackWindow.classList.add("warning");
-			showFeedback.classList.add("warning");		
-			showFeedback.innerHTML = "Atleast input a word"; 
-			break;
-		case "amountLetters":
-			feedbackWindow.classList.add("warning");
-			showFeedback.classList.add("warning");
-			showFeedback.innerHTML = userGuess+ " is not a " + amountOfLetters + " letter word.";
-			break;
-		case "existingWord":
-			feedbackWindow.classList.add("warning");
-			showFeedback.classList.add("warning");			
-			showFeedback.innerHTML = userGuess+ " is not an existing word, starting with a " +beginLetter;
-			break;
-		case "processing":
-			feedbackWindow.classList.remove("warning");
-			showFeedback.classList.remove("warning");	
-			showFeedback.innerHTML = "Processing your input...";
-			break;
-		case "winner":
-			feedbackWindow.classList.remove("warning");
-			showFeedback.classList.remove("warning");	
-			feedbackWindow.classList.add("winner");
-			showFeedback.innerHTML = "You WON !! Well done !!<br><br><b>Refresh the page to play again.</b>";
-			break;
-		case "clear":
-			feedbackWindow.classList.remove("warning");
-			showFeedback.classList.remove("warning");	
-			feedbackWindow.classList.remove("winner");
-			showFeedback.innerHTML = "Type your guess below";
-			break;
-		case "lost":
-			feedbackWindow.classList.add("warning");
-			showFeedback.classList.add("warning");
-			showFeedback.innerHTML = "Ahww you Lost !! Don't give up !!<br><br><b>Refresh the page to play again.</b>";
-			break;
-		default: //error
-			feedbackWindow.classList.add("error");
-			showFeedback.classList.add("warning");		
-			showFeedback.innerHTML = "Something went wrong, start a bug report?";
+	let theScore;
+	theScore = document.getElementsByClassName("playerScore");
+
+// Shows the player's score
+showScore();
+
+function showScore(){
+	console.log(score);
+	for (i = 0; i < theScore.length; i++){
+		theScore[i].innerHTML = score.toString();
 	}
 }
 
+// MINOR Animations for the Send button
+sendInput.addEventListener("mouseover", animBtnOver);
+sendInput.addEventListener("mouseout", animBtnOut);
+sendInput.addEventListener("mousedown", animBtnDown);
+sendInput.addEventListener("mouseup", animBtnUp);
+
+function animBtnOver(){
+	sendInput.classList.add("overBorders");
+}
+
+function animBtnOut(){
+	sendInput.classList.remove("overBorders");
+}
+
+function animBtnDown(){
+	sendInput.classList.remove("overBorders");
+	sendInput.classList.add("onClicking");
+}
+
+function animBtnUp(){
+	sendInput.classList.remove("onClicking");
+	sendInput.classList.add("overBorders");
+}
+
+
+// ********************************** //
+// -- ALL THE FUNCTIONS ARE BELOW --  //
+// ********************************** //
+
+// resets the text input to empty
+function resetInput(){
+	input.value = "";
+}
+
+// sets the colors of the letter blocks
 function setVisual(turn, index, theLetter, toType){
 	let guess = document.getElementsByClassName("row"+turn);
 	guess[index].textContent = theLetter.toUpperCase();	
@@ -71,45 +71,63 @@ function setVisual(turn, index, theLetter, toType){
 
 }
 
-function resetInput(){
-	input.value = "";
-}
 
-function playSound(){
-	return;
+	
+
+
+// gives various feedback to the user based on the game state
+function setUserFeedback(toType){
+	let feedbackWindow = document.getElementById("user_feedback");
+	let showFeedback = document.getElementById("feedback");
+	switch(toType){
+		case "noLetters":
+			feedbackWindow.classList.add("warning");
+			showFeedback.classList.add("warning");		
+			showFeedback.innerHTML = "Atleast input a word"; 
+			break;
+		case "amountLetters":
+			feedbackWindow.classList.add("warning");
+			showFeedback.classList.add("warning");
+			showFeedback.innerHTML = userGuess+ " is not a " + amountOfLetters + " letter word.";
+			break;
+		case "existingWord":
+			feedbackWindow.classList.add("warning");
+			showFeedback.classList.add("warning");			
+			showFeedback.innerHTML = userGuess+ " is not an existing " + localisation + " word, starting with a " +startingLetter;
+			break;
+		case "processing":
+			feedbackWindow.classList.remove("warning");
+			showFeedback.classList.remove("warning");	
+			showFeedback.innerHTML = "Processing your input...";
+			break;
+		case "winner":
+			feedbackWindow.classList.remove("warning");
+			showFeedback.classList.remove("warning");	
+			feedbackWindow.classList.add("winner");
+			showFeedback.innerHTML = "You WON !! Well done !!<br><br><b>Press 'Again' below to play again.</b>";
+			break;
+		case "clear":
+			feedbackWindow.classList.remove("warning");
+			showFeedback.classList.remove("warning");	
+			feedbackWindow.classList.remove("winner");
+			showFeedback.innerHTML = "Type your guess below";
+			break;
+		case "lost":
+			feedbackWindow.classList.add("warning");
+			showFeedback.classList.add("warning");
+			showFeedback.innerHTML = "Ahww you Lost !! Don't give up !!<br> The word was " + toGuess + "<br><br><b>Press 'Again' below to play again.</b>";
+			break;
+		case "gameover":
+			feedbackWindow.classList.add("warning");
+			showFeedback.classList.add("warning");
+			showFeedback.innerHTML = "This game is already over !! <br><br><b>Press 'Again' below to play again.</b>";
+			break;
+		default: //error
+			feedbackWindow.classList.add("error");
+			showFeedback.classList.add("warning");		
+			showFeedback.innerHTML = "Something went wrong, start a bug report?";
 	}
-
-
-//**** This bit should probably move to another script for siteView control ****//
-let getInfo = document.getElementById("about_btn"); // get the send button
-getInfo.addEventListener("click", showInfo);
-let theInfo = document.getElementById("instructions");
-
-function showInfo (){
-	theInfo.classList.toggle("hide");
 }
-//**** end of this bit ****//
-
-
-/* during development setVisuals grew to 3 'the same' functions, so they got turned into a switch
-function setVisualCorrect(turn, index, theLetter){
-	let guess = document.getElementsByClassName("row"+turn);	
-	guess[index].classList.add("correct");
-	guess[index].textContent = theLetter.toUpperCase();
-}
-
-function setVisualAlmost(turn, index, theLetter){
-	let guess = document.getElementsByClassName("row"+turn);		
-	guess[index].classList.add("almost");
-	guess[index].textContent = theLetter.toUpperCase();
-}
-
-function setVisualWrong(turn, index, theLetter){
-	let guess = document.getElementsByClassName("row"+turn);		
-	guess[index].classList.add("wrong");
-	guess[index].textContent = theLetter.toUpperCase();
-}
-*/
 
 
 
